975e33404f4df894f5d8414089abd279f7a0cc21
Marco Radocchia <marco.radocchia@outlook.com>
2022-01-08 16:34:11 +0100

    formatting

--- traker.py
+++ traker.py
@@ -10,7 +10,7 @@ from email.message import EmailMessage
 from fake_useragent import UserAgent
 from json import dumps as json_dumps
 from json import loads as json_loads
-from os import listdir,mkdir, rename
+from os import listdir, mkdir, rename
 from os.path import expanduser, isdir, isfile, join
 from re import compile, match, IGNORECASE
 from requests import get
@@ -31,6 +31,8 @@ USERAGENT_FALLBACK = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.11\
 TIMEOUT = 5
 
 # UTILS
+
+
 def check_mail_addr(mail: str) -> None:
     if not match(r"^[A-Za-z0-9\.\+_-]+@[A-Za-z0-9\._-]+\.[a-zA-Z]*$", mail):
         logging.error(f"'{mail}' is not a valid mail address")
@@ -46,7 +48,8 @@ def check_smtp_server(server_url: str) -> None:
 def check_url(url: str) -> None:
     url_regex = compile(
         r"^(?:http|ftp)s?://"  # http:// or https://
-        r"(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|"  # domain...
+        r"(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)"
+        r"+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|"  # domain...
         r"localhost|"  # localhost...
         r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})"  # ...or ip
         r"(?::\d+)?"  # optional port
@@ -69,42 +72,42 @@ def get_config() -> dict:
     config = ConfigParser()
     config.read(CONFIG_FILE)
     config_opts = {}
-    if 'mail' in config.sections():
-        smtp_server = config.get('mail', 'smtp_server')
+    if "mail" in config.sections():
+        smtp_server = config.get("mail", "smtp_server")
         # check server url before continue
         check_smtp_server(smtp_server)
         # check mail address before continue
-        notifier_addr = config.get('mail', 'notifier_addr')
+        notifier_addr = config.get("mail", "notifier_addr")
         check_mail_addr(notifier_addr)
-        config_opts['mail'] = {}
-        config_opts['mail']['smtp_server'] = smtp_server
-        config_opts['mail']['port'] = config.get('mail', 'port')
-        config_opts['mail']['notifier_addr'] = notifier_addr
-        config_opts['mail']['notifier_psw'] = config.get('mail', 'notifier_psw')
+        config_opts["mail"] = {}
+        config_opts["mail"]["smtp_server"] = smtp_server
+        config_opts["mail"]["port"] = config.get("mail", "port")
+        config_opts["mail"]["notifier_addr"] = notifier_addr
+        config_opts["mail"]["notifier_psw"] =\
+            config.get("mail", "notifier_psw")
     return config_opts
 
 
 def get_proxy_list() -> list:
-    print('Retrieving proxy list...')
+    print("Retrieving proxy list...")
     try:
         response = get(
-            'https://free-proxy-list.net/',
-            headers={'User-Agent': get_useragent()},
+            "https://free-proxy-list.net/",
+            headers={"User-Agent": get_useragent()},
         )
-        soup = BeautifulSoup(response.content, 'lxml')
-        table = soup.find('tbody')
+        soup = BeautifulSoup(response.content, "lxml")
+        table = soup.find("tbody")
         proxies = []
         for row in table:
-            if row.find_all('td')[4].text =='elite proxy':
-                proxy = ':'.join(
-                    [row.find_all('td')[0].text,
-                    row.find_all('td')[1].text]
+            if row.find_all("td")[4].text == "elite proxy":
+                proxy = ":".join(
+                    [row.find_all("td")[0].text, row.find_all("td")[1].text]
                 )
                 proxies.append(proxy)
         return proxies
     except Exception as e:
         logging.error(f"unable to get proxy list ({e})")
-        sys_exit(f"ERROR: unable to get proxy list")
+        sys_exit("ERROR: unable to get proxy list")
 
 
 def get_date() -> str:
@@ -114,18 +117,18 @@ def get_date() -> str:
 def get_page(url: str) -> BeautifulSoup:
     # use random user agent and rotating proxies to prevent request blocking
     proxy = get_working_proxy(proxy_list=get_proxy_list())
-    print('Contacting server...')
+    print("Contacting server...")
     try:
         page = get(
             url=url,
-            headers={'User-Agent': get_useragent()},
+            headers={"User-Agent": get_useragent()},
             proxies={
-                'http': f'http://{proxy}',
-                'https': f'http://{proxy}',
+                "http": f"http://{proxy}",
+                "https": f"http://{proxy}",
             },
             timeout=60,
         ).text
-        page = BeautifulSoup(page, 'lxml')
+        page = BeautifulSoup(page, "lxml")
     except Exception as e:
         logging.error(f"unable to load page '{url}' ({e})")
         sys_exit(f"ERROR: unable to load page '{url}'")
@@ -134,36 +137,37 @@ def get_page(url: str) -> BeautifulSoup:
 
 def get_price(page: BeautifulSoup) -> float:
     try:
-        price = page.find(class_='a-offscreen') # this is for amazon only atm
-        price = float(price.text.strip('\n')[:-1].replace(',', '.'))
+        price = page.find(class_="a-offscreen")  # this is for amazon only atm
+        price = float(price.text.strip("\n")[:-1].replace(",", "."))
         return price
     except Exception as e:
         logging.error(f"unable to retrieve price information in page ({e})")
-        sys_exit(f"ERROR: unable to retrieve price information in page")
+        sys_exit("ERROR: unable to retrieve price information in page")
 
 
 def get_working_proxy(proxy_list: list) -> str:
-    print('Looking for working proxy...')
+    print("Looking for working proxy...")
     while len(proxy_list) > 0:
         proxy = proxy_list.pop()
-        proxy_ip = proxy.split(':')[0]
+        proxy_ip = proxy.split(":")[0]
         try:
             response = get(
-                url='https://httpbin.org/ip',
+                url="https://httpbin.org/ip",
                 proxies={
-                    'http': f'http://{proxy}',
-                    'https': f'http://{proxy}',
+                    "http": f"http://{proxy}",
+                    "https": f"http://{proxy}",
                 },
                 timeout=TIMEOUT,
             ).json()
-            if response['origin'] == proxy_ip:
+            if response["origin"] == proxy_ip:
                 logging.info(f"using proxy {proxy_ip}")
                 print(f"Using proxy {proxy_ip}")
                 return proxy
-        except:
+        except Exception:
             pass
     logging.error("unable to find working proxy")
     sys_exit("ERROR: unable to find working proxy")
+
 
 def get_useragent() -> str:
     """
@@ -178,19 +182,16 @@ def get_useragent() -> str:
     need_cache_update = True
     useragents_file = join(
         XDG_DATA,
-        'useragents_' + str(date.today().month) + '.json'
+        "useragents_" + str(date.today().month) + ".json"
     )
     for file in xdg_data_files:
         if "useragents" in file:
-            if int(file.split('.json')[0].split('_')[1]) == date.today().month:
+            if int(file.split(".json")[0].split("_")[1]) == date.today().month:
                 need_cache_update = False
             else:
                 rename(join(XDG_DATA, file), useragents_file)
             break
-    ua = UserAgent(
-        fallback=USERAGENT_FALLBACK,
-        path=useragents_file
-    )
+    ua = UserAgent(fallback=USERAGENT_FALLBACK, path=useragents_file)
     # update user agent cache every month
     if need_cache_update:
         ua.update()
@@ -210,7 +211,7 @@ def check_data_dir() -> None:
 
 
 def write_list(products: list) -> None:
-    with open(PRODUCT_LIST_FILE, 'w+') as products_file:
+    with open(PRODUCT_LIST_FILE, "w+") as products_file:
         products_file.write(json_dumps(products, indent=4))
 
 
@@ -219,31 +220,31 @@ def get_list() -> list:
     if not isfile(PRODUCT_LIST_FILE):
         check_data_dir()
         write_list(products=[])
-        return [] # if file didn't exist, no need to read it
-    with open(PRODUCT_LIST_FILE, 'r') as products_file:
+        return []  # if file didn't exist, no need to read it
+    with open(PRODUCT_LIST_FILE, "r") as products_file:
         product_list = json_loads(products_file.read())
     return product_list
 
 
 # NOTIFICATIONS
 def send_notification(mail_addr: str, mail_body: str) -> None:
-    print(f'Sending mail notification to {mail_addr}')
+    print(f"Sending mail notification to {mail_addr}")
     config_opts = get_config()
-    if 'mail' in config_opts:
+    if "mail" in config_opts:
         mail = EmailMessage()
-        mail['Subject'] = "Price Traker: lower price detected"
-        mail['To'] = mail_addr
-        mail['From'] = config_opts['mail']['notifier_addr']
+        mail["Subject"] = "Price Traker: lower price detected"
+        mail["To"] = mail_addr
+        mail["From"] = config_opts["mail"]["notifier_addr"]
         mail.set_content(mail_body)
         try:
             with SMTP_SSL(
-                config_opts['mail']['smtp_server'],
-                config_opts['mail']['port'],
+                config_opts["mail"]["smtp_server"],
+                config_opts["mail"]["port"],
                 context=create_default_context(),
             ) as server:
                 server.login(
-                    config_opts['mail']['notifier_addr'],
-                    config_opts['mail']['notifier_psw'],
+                    config_opts["mail"]["notifier_addr"],
+                    config_opts["mail"]["notifier_psw"],
                 )
                 server.send_message(mail)
                 logging.info(f"mail notification sent to {mail_addr}")
@@ -263,19 +264,18 @@ def insert_product(url: str, mail_addr: str) -> None:
     product_list = get_list()
     # look for product in product_list
     for product in product_list:
-        if (product['url'] == url):
-            if mail_addr in product['followers']:
+        if product["url"] == url:
+            if mail_addr in product["followers"]:
                 warning_msg = (
-                    f"'{mail_addr}' already tracking "
-                    f"'{product['url']}...'"
+                    f"'{mail_addr}' already tracking " f"'{product['url']}...'"
                 )
                 logging.warning(warning_msg)
                 # if mail_addr already in followers array, exit
-                sys_exit( f"WARNING: {warning_msg}")
+                sys_exit(f"WARNING: {warning_msg}")
             else:
-                # product already in product_list but mail_addr not in followers
-                # add mail_addr to followers and exit
-                product['followers'].append(mail_addr)
+                # product already in product_list but mail_addr not in
+                # followers add mail_addr to followers and exit
+                product["followers"].append(mail_addr)
                 logging.info(
                     f"'{mail_addr}' started tracking "
                     f"'{product['title']}' "
@@ -286,31 +286,23 @@ def insert_product(url: str, mail_addr: str) -> None:
     # if product not found in product_list, add new entry to product_list
     page = get_page(url=url)
     try:
-        title = page.find(id='productTitle').text.strip(' \n') # TODO amazon only atm
+        # TODO amazon only atm
+        title = page.find(id="productTitle").text.strip(" \n")
     except Exception as e:
         logging.error(f"no id found at given url page ({e})")
-        sys_exit(f"ERROR: no id found at given url page")
+        sys_exit("ERROR: no id found at given url page")
     product_list.append(
         {
-            'url': url,
-            'title': title,
-            'followers': [mail_addr],
-            'prices': [
-                {
-                    'date': get_date(),
-                    'price': get_price(page)
-                }
-            ],
+            "url": url,
+            "title": title,
+            "followers": [mail_addr],
+            "prices": [{"date": get_date(), "price": get_price(page)}],
         }
     )
-    logging.info(
-        f"'{mail_addr}' started tracking "
-        f"'{title}' "
-        f"({url})"
-    )
+    logging.info(f"'{mail_addr}' started tracking " f"'{title}' " f"({url})")
     # write updated product_list to PRODUCT_LIST_FILE
     write_list(products=product_list)
-    print('Done')
+    print("Done")
 
 
 def list_products() -> None:
@@ -321,7 +313,7 @@ def list_products() -> None:
             f"│  ├── {product['prices'][-1]['price']}€\n"
             f"│  ├── {product['prices'][-1]['date']}\n"
             f"│  └── {', '.join(product['followers'])}"
-        ) 
+        )
 
 
 def remove_product(substr: str, mail_addr: str) -> None:
@@ -329,14 +321,17 @@ def remove_product(substr: str, mail_addr: str) -> None:
     check_mail_addr(mail_addr)
     product_list = get_list()
     for product in product_list:
-        if substr.lower() in product['title'].lower():
-            if mail_addr not in product['followers']:
+        if substr.lower() in product["title"].lower():
+            if mail_addr not in product["followers"]:
                 return
-            if input(
+            if (
+                input(
                     f"Remove '{mail_addr}' from "
                     f"'{product['title']}' followers list? [y/N]: "
-                ).lower() == 'y':
-                product['followers'].remove(mail_addr)
+                ).lower()
+                == "y"
+            ):
+                product["followers"].remove(mail_addr)
                 write_list(products=product_list)
                 logging.info(
                     f"'{mail_addr}' stopped tracking "
@@ -364,17 +359,12 @@ def update_prices() -> None:
     for product in product_list:
         # if the last price update is today,
         # ignore updating price for that product
-        if product['prices'][-1]['date'] == today:
+        if product["prices"][-1]["date"] == today:
             continue
-        page = get_page(url=product['url'])
+        page = get_page(url=product["url"])
         today_price = get_price(page)
-        product['prices'].append(
-            {
-                'date': today,
-                'price': today_price
-            }
-        )
-        prev_price = product['prices'][-2]['price']
+        product["prices"].append({"date": today, "price": today_price})
+        prev_price = product["prices"][-2]["price"]
         price_delta = round(today_price - prev_price, 2)
         # if product has lower price than last check,
         # add followers to notification_queue
@@ -390,7 +380,7 @@ def update_prices() -> None:
             # for each follower of the product, append the notification_body
             # if it already exists in the notification_queue,
             # create an instance otherwhise
-            for follower in product['followers']:
+            for follower in product["followers"]:
                 if follower in notification_queue:
                     notification_queue[follower] += notification_body
                 else:
@@ -403,54 +393,58 @@ def update_prices() -> None:
             mail_addr=mail_addr,
             mail_body=notification_queue[mail_addr],
         )
-    print('Done')
+    print("Done")
 
 
 # MAIN
 def main() -> None:
     # if log file doesn't exist, create it
     if not isfile(LOG_FILE):
-        check_data_dir() # check if data directory exists
-        with open(LOG_FILE, 'w+') as logfile:
-            logfile.writelines(['-- PRICE TRAKER LOG --\n'])
+        check_data_dir()  # check if data directory exists
+        with open(LOG_FILE, "w+") as logfile:
+            logfile.writelines(["-- PRICE TRAKER LOG --\n"])
     # configuring logging
     logging.basicConfig(
         filename=LOG_FILE,
-        encoding='utf-8',
+        encoding="utf-8",
         format=LOG_FORMAT,
-        datefmt='%Y-%m-%d [%H:%M:%S]',
-        level=20, # INFO level
+        datefmt="%Y-%m-%d [%H:%M:%S]",
+        level=20,  # INFO level
     )
     argparser = ArgumentParser(allow_abbrev=False)
     argparser.add_argument(
-        '-i', '--insert',
+        "-i",
+        "--insert",
         type=str,
         nargs=2,
-        metavar=('<url>', '<mail>'),
-        help= (
-            '<mail> starts tracking <url> '
-            '(requires valid mail option for noticitation purposes)'
-        )
+        metavar=("<url>", "<mail>"),
+        help=(
+            "<mail> starts tracking <url> "
+            "(requires valid mail option for noticitation purposes)"
+        ),
     )
     argparser.add_argument(
-        '-l', '--list',
-        action='store_true',
-        help='list all the tracked products'
+        "-l",
+        "--list",
+        action="store_true",
+        help="list all the tracked products"
     )
     argparser.add_argument(
-        '-r', '--remove',
+        "-r",
+        "--remove",
         type=str,
         nargs=2,
-        metavar=('<title_substr>', '<mail>'),
-        help= (
-            '<mail> stops tracking <title_substr> '
-            '(<title_substr> indicates a substring of the product title)'
-        )
+        metavar=("<title_substr>", "<mail>"),
+        help=(
+            "<mail> stops tracking <title_substr> "
+            "(<title_substr> indicates a substring of the product title)"
+        ),
     )
     argparser.add_argument(
-        '-u', '--update',
-        action='store_true',
-        help='update prices for every product'
+        "-u",
+        "--update",
+        action="store_true",
+        help="update prices for every product"
     )
 
     if len(argv) == 1:  # If no argument is given print help and exit
@@ -469,5 +463,5 @@ def main() -> None:
         list_products()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
